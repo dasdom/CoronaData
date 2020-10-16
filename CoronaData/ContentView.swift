247a5812dc7bcf66c39a89696f4c630f4076e27f
Dominik Hauser <dom@dasdom.de>
2020-10-16 10:27:14 +0200

    Initial Commit

---
+++ CoronaData/ContentView.swift
@@ -0,0 +1,18 @@
+//  Created by dasdom on 16.10.20.
+//  
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CoronaData/CoronaDataApp.swift
@@ -0,0 +1,14 @@
+//  Created by dasdom on 16.10.20.
+//  
+//
+
+import SwiftUI
+
+@main
+struct CoronaDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
